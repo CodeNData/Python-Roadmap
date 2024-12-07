da4642ac245ad241ef39048efbb79ee0be99e97d
Parth Saxena <137363843+ghosty15@users.noreply.github.com>
2024-12-07 23:55:03 +0530

    Reference Counting
    Today I studied about Memory Allocation and there types

---
+++ refrence.py
@@ -0,0 +1,23 @@
+import sys
+
+def check(a, b):
+    print("a = ", a)
+    print("b = ", b)
+    if id(a) == id(b):
+        print("a and b refer to the same object")
+    else:
+        print("a and b dont refer to the same object")
+
+a = [1, 2, 3]
+print("Count of references = ", sys.getrefcount(a))
+print()
+
+b = a
+check(a, b)
+print("Count of references = ", sys.getrefcount(a))
+print()
+
+b = 5
+check(a, b)
+print("Count of references = ", sys.getrefcount(a))
+print()
