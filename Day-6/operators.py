e78724abb9a92879d6ec06d0a0f8efee34c439be
Parth Saxena <137363843+ghosty15@users.noreply.github.com>
2024-12-11 23:19:51 +0530

    Operators
    Today i explored all the operators in python although i haven't mentioned all type of operators.

---
+++ Day-6/operators.py
@@ -0,0 +1,36 @@
+x = 5
+y = 7
+#Arithmetic Operators
+print(x+y) #addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x//y) #floor division
+print(x%y) #modulo
+print(x**y) #Exponential
+
+#Comparison Operator
+print(x<y)#less than
+print(x>y)#greater than
+print(x==y)#equals to
+print(x!=y)#Not Equals to
+print(x<=y)#less than equals too
+print(x>=y)#greater than equals too
+
+#Logical Operators
+if(45/9 and 40/8): #and operator
+    print("Both conditions are True")
+if(45/9 or 40/5): #or operator
+    print("Only one condition needs to be true")
+z = 1 #not operator
+print(not z)
+
+#Bitwise Operator
+a= 5
+b= 3
+print(a&b) #bitwise and
+print(a|b) #bitwise or
+print( ~a) #bitwise not
+print(a^b) #bitwise xor
+print(a>>b) #bitwise right shift
+print(a<<b) #bitwise left shift
