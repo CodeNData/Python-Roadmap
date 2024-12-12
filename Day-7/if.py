aded61c0966873ba21406c4b6d03e4b0570530d5
Parth Saxena <137363843+ghosty15@users.noreply.github.com>
2024-12-12 23:18:19 +0530

    if/else & loops
    Today i learned about if/else & loops

---
+++ Day-7/if.py
@@ -0,0 +1,12 @@
+x = 20
+y = 40
+if(x<y): #Prints the code only if given condition is true
+    print(x, "is smaller than", y)
+a = 35
+b = 30
+if(x<y):
+    print(a, "is smaller than", b)
+elif(x==y): #runs after if statement when if statement is false
+    print(a,"equals to ",b)
+else: #if ' if statement' and elif statements are false it comes to else statement
+    print(a, "is greater than", b)
