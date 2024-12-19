c5b7e546cd39b3625454dc2174854a3b116f287a
Parth Saxena <137363843+ghosty15@users.noreply.github.com>
2024-12-19 23:24:51 +0530

    Function
    I explored Functions in Python

---
+++ Day-9/func.py
@@ -0,0 +1,7 @@
+def sum(x , y):
+    return x + y
+from func import sum
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+output = sum(a,b)
+print("The output is:", output)
