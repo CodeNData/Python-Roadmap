807a7efcb61b74b9220973d97e18861f30d6e4ca
Parth Saxena <137363843+ghosty15@users.noreply.github.com>
2024-12-25 23:21:42 +0530

    Functions of List
    I explored different Functions of Lists

--- Day-11/list.py
+++ Day-11/list.py
@@ -1,2 +1,18 @@
-list1 = ['My','name','is','Parth','Saxena']
+list1 = ['My','name','is','Parth','Saxena'] #list created
 print(list1)
+list1.append('😎') #append function is used to add a new element to the list's end
+print(list1)#it will print updated list
+newList = list1.copy() #copy function is used tocopy one list to another
+list1.clear()#All elements in List are removed
+print(list1,newList)
+list2 = ['I','am','a','Student.']
+newList.extend(list2) #Extend function is used to add another list to current list
+print(newList)
+index = newList.index('Parth') #Index function is used to return index of specified value.
+print(index)
+newList.pop(5)#Pop function is used to pop an element out of the list.
+print(newList)
+newList.reverse()#The list is reordered reversely
+print(newList)
+newList.sort()#Sorts the list
+print(newList)
