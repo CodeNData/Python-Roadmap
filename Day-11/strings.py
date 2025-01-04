f75e093d52cf96d5f33d702ac9301dc20256e9b0
Parth Saxena <bls.parthsaxena.xia2021@gmail.com>
2025-01-04 23:24:17 +0530

    Strings

---
+++ Day-11/strings.py
@@ -0,0 +1,9 @@
+# Single Quotes: 'Hello, World!'
+# Double Quotes: "Hello, World!"
+# Triple Quotes: '''Hello, World!''' or """Hello, World!"""
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+print(len(a))
