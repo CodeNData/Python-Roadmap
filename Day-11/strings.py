83f061fe1fe7e11ff76843b08091776165130fff
Parth Saxena <bls.parthsaxena.xia2021@gmail.com>
2025-01-09 23:22:13 +0530

    Strings.py

--- Day-11/strings.py
+++ Day-11/strings.py
@@ -7,3 +7,10 @@ sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
 print(len(a))
+print(a[1:4])  #slicing method is used to access a range of characters in the String.
+print(a[2:10])
+print(a[2:-1])
+b = "I am a learner"
+print(b)
+for x in b:
+    print(x)  #we can use a for loop to traverse through the characters in a string.
