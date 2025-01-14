4edfe9562996c61e5ea2d6eab5658a2a27f632e1
Parth Saxena <bls.parthsaxena.xia2021@gmail.com>
2025-01-14 21:18:05 +0530

    Strings.py
    Concatenation methods in Strings

--- Day-11/strings.py
+++ Day-11/strings.py
@@ -14,5 +14,15 @@ b = "I am a learner"
 print(b)
 for x in b:
     print(x)  #we can use a for loop to traverse through the characters in a string.
-res = a.replace("Lorem", "Code")
+res = a.replace("Lorem", "Code")#Replace method is used to replace from string
 print(res)
+#Different Concatenation methods in String
+c = a + b #first method
+print(c)
+d = " ".join([a,b]) #second method
+print("% s % s" % (a,b)) #third method
+e =  "{} {}".format(a, b) #fourth method
+print(e)
+print(a,b) #fifth method
+f =  f'{a} {b}' #sixth method
+print(f)
