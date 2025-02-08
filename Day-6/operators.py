8813ff520780d5f07611bf1cf420badea2812586
Parth Saxena <bls.parthsaxena.xia2021@gmail.com>
2025-02-08 22:44:27 +0530

    Operators.py

--- Day-6/operators.py
+++ Day-6/operators.py
@@ -9,6 +9,7 @@ print(x//y) #floor division
 print(x%y) #modulo
 print(x**y) #Exponential
 
+
 #Comparison Operator
 print(x<y)#less than
 print(x>y)#greater than
@@ -17,6 +18,7 @@ print(x!=y)#Not Equals to
 print(x<=y)#less than equals too
 print(x>=y)#greater than equals too
 
+
 #Logical Operators
 if(45/9 and 40/8): #and operator
     print("Both conditions are True")
@@ -24,6 +26,7 @@ if(45/9 or 40/5): #or operator
     print("Only one condition needs to be true")
 z = 1 #not operator
 print(not z)
+
 
 #Bitwise Operator
 a= 5
